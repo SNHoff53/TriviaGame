799a901f815c816142e656c8fb04bd67deb76d7e
hoffs <stephanie.nhoffman7@gmail.com>
2019-09-24 21:20:13 -0700

    added html/Bootstrap and little CSS for structure and layout of game

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+// Variables
+
+// Begin with a start button
+// event button .on("click" function())
+
+// clock starts to coutdown
+// Time Remaining: 
+
+// 5 triva questions are displayed on the page
+
+// radio dials
+    // note: you can only click on one answer -- never two
+
+// when timer hits 0 seconds, the game is over
+
+// when game is finished
+    // display Correct: (#)
+    // display Incorrect: (#)
+    // Unanswered: (#) -- total number of questions - questions not clicked
+
+    
+
