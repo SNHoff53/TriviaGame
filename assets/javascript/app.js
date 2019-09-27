81e29c9178f38b9bc8d78a8834f08d7706fe87c4
hoffs <stephanie.nhoffman7@gmail.com>
2019-09-27 11:28:07 -0700

    added trivia questions, and fixed radio button click issues. Now you can click one answer per question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,22 +6,6 @@
 // clock starts to coutdown
 // Time Remaining: 
 
-// 5 triva questions are displayed on the page
-
-// 1. What type of galaxy is the most common in the universe? Elliptical galaxies
-// 8. What is the smallest planet in our solar system? Mercury
-// 9. What is the most common type of star found in the Milky Way? Red dwarf stars
-// 15. What is the farthest distance from Earth a manned mission has traveled? (Plus or minus 20,000 miles.) 248,655 miles
-// 21. What are the dimensions in inches of the first footprint on Earth’s moon? 13 by 6 inches
-// 4. How old is the universe in years? (Plus or minus 1 billion years.) 13.8 billion years old
-// 5. What percent of the universe is dark matter? (Plus or minus 2%.) 27%
-// 16. How many minutes was the shortest space flight? 15 minutes
-// 18. How many moons does Jupiter have? 67
-// 22. What flavor ice cream did Baskin-Robbins release in 1969 to commemorate America’s landing on the moon? Lunar Cheesecake
-
-
-// radio dials
-    // note: you can only click on one answer -- never two
 
 // when timer hits 0 seconds, the game is over
 
